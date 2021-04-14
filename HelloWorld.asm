db2d9a04941fdaf5331a26c5799f9369fc36fbc7
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2021-04-15 01:10:58 +0300

    Create HelloWorld.asm

---
+++ HelloWorld.asm
@@ -0,0 +1,9 @@
+; Author: Talson Slayman 
+
+global _start
+
+section .text                   ; the section defines the code
+_start:                         ; define an entry point. Like the main func in a C program.
+
+section .data                   ; the section holdes variables
+  message: db "Hello World!"    ;db for Define Data
