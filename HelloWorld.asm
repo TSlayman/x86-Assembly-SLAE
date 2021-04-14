a0a243923f499f6a172534967df1628092c6a721
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2021-04-15 01:12:04 +0300

    Update HelloWorld.asm

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -6,4 +6,4 @@ section .text                   ; the section defines the code
 _start:                         ; define an entry point. Like the main func in a C program.
 
 section .data                   ; the section holdes variables
-  message: db "Hello World!"    ;db for Define Data
+  message: db "Hello World!"    ; db for Define Data
