28b715b536680f6daadd6275e2b079d8ae293657
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2021-04-15 01:31:55 +0300

    Update HelloWorld.asm

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -4,6 +4,16 @@ global _start
 
 section .text                   ; the section defines the code
 _start:                         ; define an entry point. Like the main func in a C program.
-
+  mov eax, 0x4                  ; invoke syscall stdin to eax reg. 
+  mov ebx, 0x1                  ; invoke syscall stdout to ebx reg.
+  mov exc, message              ; point to the "message" string.
+  mov edx, mlen                 ; move message lengths to edx reg.
+  int 0x80                      ; issue systemcall
+  
+  mov eax, 0x1
+  mov ebx, 0x5
+  int 0x80                      ; exit the program gracefully
+  
 section .data                   ; the section holdes variables
   message: db "Hello World!"    ; db for Define Data
+  mlen  eq $-message            ; define the byte lengths of the message
